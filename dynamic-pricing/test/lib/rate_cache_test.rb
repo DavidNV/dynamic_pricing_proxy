62fd025f423ada88fa1c49e7e284dbb0c2161781
DavidNV <david.nvalderrama@gmail.com>
2026-03-06 04:12:24 +0900

    Feat: add test for quota counter and update write to receive TTL parameter

--- dynamic-pricing/test/lib/rate_cache_test.rb
+++ dynamic-pricing/test/lib/rate_cache_test.rb
@@ -6,12 +6,12 @@ class RateCacheTest < ActiveSupport::TestCase
   VALID_ROOM   = "SingletonRoom"
   VALID_RATE   = { "period" => VALID_PERIOD, "hotel" => VALID_HOTEL, "room" => VALID_ROOM, "rate" => "15000" }.freeze
 
-  def cache_key
+  def rate_cache_key
     "pricing:#{VALID_PERIOD}:#{VALID_HOTEL}:#{VALID_ROOM}"
   end
 
   setup do
-    REDIS.del(cache_key)
+    REDIS.del(rate_cache_key)
   end
 
   test "returns nil on a cache miss" do
@@ -24,5 +24,31 @@ class RateCacheTest < ActiveSupport::TestCase
     result = RateCache.fetch(period: VALID_PERIOD, hotel: VALID_HOTEL, room: VALID_ROOM)
 
     assert_equal VALID_RATE, result
+  end
+
+  test "returns nil after TTL expires" do
+    RateCache.write(period: VALID_PERIOD, hotel: VALID_HOTEL, room: VALID_ROOM, rate: VALID_RATE, ttl: 1)
+    sleep 1.1
+    result = RateCache.fetch(period: VALID_PERIOD, hotel: VALID_HOTEL, room: VALID_ROOM)
+
+    assert_nil result
+  end
+
+  test "increments the quota counter on each upstream call" do
+    REDIS.del("upstream:call_count")
+
+    RateCache.increment_quota_counter
+    RateCache.increment_quota_counter
+    RateCache.increment_quota_counter
+
+    assert_equal 3, RateCache.quota_count
+  end
+
+  test "detects when quota is exceeded" do
+    REDIS.del("upstream:call_count")
+
+    1000.times { RateCache.increment_quota_counter }
+
+    assert RateCache.quota_exceeded?
   end
 end
