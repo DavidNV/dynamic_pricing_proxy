c803b2a910037157ad2e419ce1dd14e252274d51
DavidNV <david.nvalderrama@gmail.com>
2026-03-05 17:25:13 +0900

    Feat: implement full rate object return in PricingService

--- dynamic-pricing/app/services/api/v1/pricing_service.rb
+++ dynamic-pricing/app/services/api/v1/pricing_service.rb
@@ -11,7 +11,7 @@ module Api::V1
       rate = RateApiClient.get_rate(period: @period, hotel: @hotel, room: @room)
       if rate.success?
         parsed_rate = JSON.parse(rate.body)
-        @result = parsed_rate['rates'].detect { |r| r['period'] == @period && r['hotel'] == @hotel && r['room'] == @room }&.dig('rate')
+        @result = parsed_rate['rates'].detect { |r| r['period'] == @period && r['hotel'] == @hotel && r['room'] == @room }
       else
         errors << rate.body['error']
       end
