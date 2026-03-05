4a34f3662c4682172d47a48b99395f4f166273ec
DavidNV <david.nvalderrama@gmail.com>
2026-03-06 04:51:53 +0900

    Refactor: Tiny update on RateCache

--- dynamic-pricing/lib/rate_cache.rb
+++ dynamic-pricing/lib/rate_cache.rb
@@ -1,34 +1,38 @@
 class RateCache
-  TTL = 300 # 5 minutes in seconds
+  TTL         = 300    # 5 minutes
   QUOTA_KEY   = "upstream:call_count"
-  QUOTA_LIMIT = 1000
-  QUOTA_TTL   = 86400
+  QUOTA_LIMIT = ENV.fetch("UPSTREAM_QUOTA_LIMIT", 1000).to_i # calls per day
+  QUOTA_TTL   = ENV.fetch("UPSTREAM_QUOTA_TTL", 86400).to_i  # 24 hours
 
-  def self.fetch(period:, hotel:, room:)
-    cached = REDIS.get(cache_key(period, hotel, room))
-    JSON.parse(cached) if cached
-  end
+  class << self
+    def fetch(period:, hotel:, room:)
+      cached = REDIS.get(cache_key(period, hotel, room))
+      JSON.parse(cached) if cached
+    end
 
-  def self.write(period:, hotel:, room:, rate:, ttl: TTL)
-    REDIS.setex(cache_key(period, hotel, room), ttl, rate.to_json)
-  end
+    def write(period:, hotel:, room:, rate:, ttl: TTL)
+      REDIS.setex(cache_key(period, hotel, room), ttl, rate.to_json)
+    end
 
-  def self.increment_quota_counter
-    # Increment the call count and set TTL if this is the first call of the day
-    # https://redis.io/docs/latest/commands/incr/
-    count = REDIS.incr(QUOTA_KEY)
-    REDIS.expire(QUOTA_KEY, QUOTA_TTL) if count == 1
-  end
+    def increment_quota_counter
+      # Increment the call count and set TTL if this is the first call of the day
+      # https://redis.io/docs/latest/commands/incr/
+      count = REDIS.incr(QUOTA_KEY)
+      REDIS.expire(QUOTA_KEY, QUOTA_TTL) if count == 1
+    end
 
-  def self.quota_count
-    REDIS.get(QUOTA_KEY).to_i
-  end
+    def quota_count
+      REDIS.get(QUOTA_KEY).to_i
+    end
 
-  def self.quota_exceeded?
-    quota_count >= QUOTA_LIMIT
-  end
+    def quota_exceeded?
+      quota_count >= QUOTA_LIMIT
+    end
 
-  private_class_method def self.cache_key(period, hotel, room)
-    "pricing:#{period}:#{hotel}:#{room}"
+    private
+
+    def cache_key(period, hotel, room)
+      "pricing:#{period}:#{hotel}:#{room}"
+    end
   end
 end
