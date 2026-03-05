19cd30e7e61dab89fc9b36293ce29dddcf503836
DavidNV <david.nvalderrama@gmail.com>
2026-03-05 19:22:17 +0900

    Feat: Add coverage forsuccessful response but malformed body response

--- dynamic-pricing/app/services/api/v1/pricing_service.rb
+++ dynamic-pricing/app/services/api/v1/pricing_service.rb
@@ -8,8 +8,13 @@ module Api::V1
 
     def run
       rate = RateApiClient.get_rate(period: @period, hotel: @hotel, room: @room)
+
       if rate.success?
         parsed_rate = JSON.parse(rate.body)
+        unless parsed_rate['rates'].is_a?(Array)
+          errors << "unexpected response from upstream service"
+          return
+        end
         @result = parsed_rate['rates'].detect { |r| r['period'] == @period && r['hotel'] == @hotel && r['room'] == @room }
         errors << "Rate not found for the requested period, hotel and room" unless @result
       else
