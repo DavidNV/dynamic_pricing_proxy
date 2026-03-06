a0488bd4fe2327238b70323b5a4211acb01b426d
DavidNV <david.nvalderrama@gmail.com>
2026-03-06 12:57:00 +0900

    Feat: add loggin as recommenden in outter README

--- dynamic-pricing/config/routes.rb
+++ dynamic-pricing/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get  '/pricing', to: 'pricing#index'
       post '/pricing', to: 'pricing#create'
+      get  '/health',  to: 'health#show'
     end
   end
 end
